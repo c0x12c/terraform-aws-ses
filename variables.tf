175ddf0a9149326f56e144e4fccbbbc35e1740ec
Duc Duong <duc.duong@c0x12c.com>
2025-06-18 12:19:55 +0700

    Remove outgoing email logs and Datadog integrations

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "environment" {
-  description = "Environment name to be used on the resource group name construction."
-  type        = string
-  default     = null
-}
-
 variable "email_domain" {
   description = "The domain name for which SES will be configured (e.g., 'example.com')."
   type        = string
@@ -11,22 +5,21 @@ variable "email_domain" {
 
 variable "emails" {
   description = "List of email addresses allowed for sending in SES sandbox mode. These are verified to send emails during testing."
-  type        = list(string)
-  default     = []
+  type = list(string)
+  default = []
 }
 
 variable "principal_roles" {
   description = "List of IAM principal roles that should have access to SES."
-  type        = list(string)
+  type = list(string)
   default     = null
 }
 
 variable "iam_role_ids" {
-  type        = list(string)
-  default     = []
+  type = list(string)
+  default = []
   description = "List of IAM role ids that should have access to SES. These roles will have permissions to send SES email."
 }
-
 
 variable "record_type" {
   description = "The record type. Valid values are A, AAAA, CAA, CNAME, DS, MX, NAPTR, NS, PTR, SOA, SPF, SRV and TXT."
@@ -68,47 +61,4 @@ variable "enabled_ses_identity_policy" {
   description = "Whether to enable the SES identity policy."
   type        = bool
   default     = true
-}
-
-variable "enabled_outgoing_email_logs" {
-  description = "Whether to enable the SES outgoing email logs."
-  type        = bool
-  default     = false
-}
-
-variable "enabled_outgoing_email_logs_cloudwatch" {
-  description = "Whether to enable the SES outgoing email logs on CloudWatch."
-  type        = bool
-  default     = false
-}
-
-variable "datadog_api_key" {
-  description = "The datadog api key"
-  type        = string
-  sensitive   = true
-  default     = null
-}
-
-variable "datadog_site" {
-  description = "The datadog site"
-  type        = string
-  default     = "datadoghq.com"
-}
-
-variable "enabled_datadog_dashboard" {
-  description = "Whether to enable the Datadog dashboard."
-  type        = bool
-  default     = false
-}
-
-variable "datadog_dashboard_environments" {
-  description = "The environments to enable the Datadog dashboard for."
-  type        = list(string)
-  default     = ["dev", "prod"]
-}
-
-variable "datadog_dashboard_default_environment" {
-  description = "The default environments to enable the Datadog dashboard for."
-  type        = string
-  default     = "prod"
 }
